2aaef29c135499644d4168ef339cc66e76892998
zhangmad <zhangmad.bj@gmail.com>
2016-08-24 09:15:16 +0800

    new

---
+++ demo/app/controllers/events_controller.rb
@@ -0,0 +1,25 @@
+class EventsController < ApplicationController
+  
+  def index
+    @events = Event.all
+  end
+  
+  def new
+    @event = Event.new
+  end
+  
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    
+    redirect_to :action => :index
+  end
+  
+  
+  private
+  
+  def event_params
+    params.require(:event).permit(:name, :description)
+  end
+  
+end
